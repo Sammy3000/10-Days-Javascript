afbd4bf1d05e0b7845c5cc0870b6ebd29712cf05
Sammy3000 <towettsammy30@gmail.com>
2024-02-11 15:23:21 +0300

    Assign the values of b to a

--- sheet1.js
+++ sheet1.js
@@ -10,3 +10,4 @@ var a; //Declaring a variable
 var b = 9; //Declaring and assigning a variable within the same line
 a =8; // Assigning a variable
 
+b = a // This assigns the contens of a to b variable
