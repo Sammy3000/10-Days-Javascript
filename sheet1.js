917487d055f18fedf851c99ad0275978c0ab91f3
Sammy3000 <towettsammy30@gmail.com>
2024-02-16 10:47:28 +0300

    Write a switch case statement

--- sheet1.js
+++ sheet1.js
@@ -79,7 +79,15 @@ function trueorFalse(age){
 
 //Switch case
 function caseInSwitch(val){
-  
+    switch(val){
+      case 1: return "alpha";
+      case 2: return "beta";
+      case 3: return "gamma";
+      case 4: return "delta";
+    }
+    
+
+
 }
 
 
