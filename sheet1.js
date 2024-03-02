da03a2e9b8f7015cc59ee178f894927a576be8c1
Sammy3000 <towettsammy30@gmail.com>
2024-03-02 21:14:01 +0300

    Creating strings as object

--- sheet1.js
+++ sheet1.js
@@ -189,3 +189,4 @@ const string1 = "A string primitive";
 const string2 = 'Also a string primitive';
 const string3 = `Yet another string primitive`;
 
+const string4 = new String("A String object");
