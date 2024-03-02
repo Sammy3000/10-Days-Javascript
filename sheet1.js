d0694bcea9dcf0ce39d1acc7fb69f1b241c7839b
Sammy3000 <towettsammy30@gmail.com>
2024-03-02 21:18:06 +0300

    Character access

--- sheet1.js
+++ sheet1.js
@@ -190,3 +190,7 @@ const string2 = 'Also a string primitive';
 const string3 = `Yet another string primitive`;
 
 const string4 = new String("A String object");
+
+//Character Access
+//There are two ways to access an individual character in a string
+
