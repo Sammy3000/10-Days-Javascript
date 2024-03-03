e44f383aa181dca01b6271386a79db8620ca17ec
Sammy3000 <towettsammy30@gmail.com>
2024-03-03 12:24:36 +0300

    Final reversed name

--- sheet1.js
+++ sheet1.js
@@ -236,6 +236,10 @@ console.log(newname);
 let reversedname = newname.reverse();
 console.log(reversedname);
 
+// Join method creates and returns a new string by concatenating all of the elements in this array, separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
+
+let finalName = reversedname.join('')
+console.log(finalName);
 
 
 
