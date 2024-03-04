3a43cafec2b17e6d246010fb2e906b2477fef52d
Sammy3000 <towettsammy30@gmail.com>
2024-03-04 08:58:14 +0300

    Notes on this

--- sheet1.js
+++ sheet1.js
@@ -242,6 +242,8 @@ let finalName = reversedname.join('')
 console.log(finalName);
 
 //This Keyword
+//A function's this keyword behaves a little differently in JavaScript compared to other languages. It also has some differences between strict mode and non-strict mode.
+
 
 
 
