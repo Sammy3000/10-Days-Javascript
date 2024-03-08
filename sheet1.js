67ddaaf125cb0f3459fc2b61d6c03194e8fc93a7
Sammy3000 <towettsammy30@gmail.com>
2024-03-08 14:11:05 +0300

    Promises code sample

--- sheet1.js
+++ sheet1.js
@@ -309,6 +309,21 @@ function mySecond() {
 //JavaScript Promise Object
 //A Promise contains both the producing code and calls to the consuming code:
 
+let myPromise = new Promise(function(myResolve, myReject) {
+  // "Producing Code" (May take some time)
+  
+    myResolve(); // when successful
+    myReject();  // when error
+  });
+  
+  // "Consuming Code" (Must wait for a fulfilled Promise)
+  myPromise.then(
+    function(value) { /* code if successful */ },
+    function(error) { /* code if some error */ }
+  );
+
+
+
 
 
 
